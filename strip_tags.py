e000125d7d12416118a886dd5f5af427d75c9d29
Dan Goldin <dangoldin@gmail.com>
2015-03-02 22:37:06 -0500

    Script to remove HTML tags

---
+++ strip_tags.py
@@ -0,0 +1,28 @@
+import sys, os
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+if __name__ == '__main__':
+    fn = sys.argv[1]
+
+    with open(fn, 'r') as f:
+        s = f.read()
+        clean = strip_tags(s)
+        with open(fn + '.new', 'w') as g:
+            g.write(clean)
+
+    os.rename(fn + '.new', fn)
+
