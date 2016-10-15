de827442f547af5f54bb68b5c66bd4174b95d9a9
Dan Goldin <dangoldin@gmail.com>
2016-10-14 21:43:42 -0400

    Super simple script to generate a password

---
+++ generate_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import random, string
+
+def generate_password(l):
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(l))
+
+if __name__ == '__main__':
+    l = 32
+    if len(sys.argv) > 1:
+        l = int(sys.argv[1])
+
+    print generate_password(l)
