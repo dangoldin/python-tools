82d6eaf6768b5f1a2270e1b31fe3a860f316f9e9
Dan Goldin <dangoldin@gmail.com>
2016-10-27 22:43:01 -0400

    Simple multi series support

--- plotter.py
+++ plotter.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
     # Assume first row is the header row
     rows = sys.stdin.readlines()
 
-    header = rows[0]
+    header = rows[0].strip().split("\t")
+
+    print 'Header', header
 
     # Assume first column is the x axis (keep as text)
     xs = []
@@ -26,7 +28,26 @@ if __name__ == '__main__':
     data = np.loadtxt(c)
 
     # Super minimal plot
-    plt.plot(data[:,0])
-    plt.xticks(np.arange(len(xs)), xs, rotation=70)
-    plt.locator_params(axis='x', nbins=10)
+    # plt.style.use('ggplot')
+    plt.style.use('fivethirtyeight')
+    # plt.style.use('bmh')
+    plt.figure(1, figsize=(20,10))
+
+    dims = data.shape
+    first = None
+    for series in range(dims[1]):
+        if series == 0:
+            sub = plt.subplot(2,1,series+1)
+            plt.setp(sub.get_xticklabels(), visible=False)
+        else:
+            plt.subplot(2,1,series+1,sharex=sub)
+
+        plt.plot(data[:,series])
+        plt.ylabel(header[series+1])
+        plt.locator_params(axis='x', nbins=10)
+
+        if series == (dims[1] - 1):
+            plt.xticks(np.arange(len(xs)), xs, rotation=70)
+
+    plt.tight_layout()
     plt.show()
