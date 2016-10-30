47be6a7c59efda8055331211f5a7ea0a3433cb0b
Dan Goldin <dangoldin@gmail.com>
2016-10-29 22:20:53 -0400

    Minor changes

--- plotter.py
+++ plotter.py
@@ -30,26 +30,26 @@ def read():
 if __name__ == '__main__':
     header, xs, data = read()
 
-    # Super minimal plot
     # plt.style.use('ggplot')
-    plt.style.use('fivethirtyeight')
     # plt.style.use('bmh')
-    plt.figure(1, figsize=(20,10))
+    plt.style.use('fivethirtyeight')
+    plt.figure(1, figsize=(20, 10))
 
-    dims = data.shape
+    n_rows, n_cols = data.shape
     first = None
-    for series in range(dims[1]):
+    for series in range(n_cols):
         if series == 0:
-            sub = plt.subplot(2,1,series+1)
+            sub = plt.subplot(2, 1, series + 1)
             plt.setp(sub.get_xticklabels(), visible=False)
         else:
-            plt.subplot(2,1,series+1,sharex=sub)
+            plt.subplot(2, 1, series + 1, sharex=sub)
 
         plt.plot(data[:,series])
-        plt.ylabel(header[series+1])
+        plt.ylabel(header[series + 1])
         plt.locator_params(axis='x', nbins=10)
 
-        if series == (dims[1] - 1):
+        # The last one
+        if series == (n_cols - 1):
             plt.xticks(np.arange(len(xs)), xs, rotation=70)
 
     plt.tight_layout()
