6391df078b374f4e0039a58850cf066cf61a4017
Dan Goldin <dgoldin@triplelift.com>
2017-09-23 22:49:26 -0400

    Clean code up

--- analyze_history.py
+++ analyze_history.py
@@ -8,19 +8,23 @@ from collections import Counter
 RE_WHITESPACE = re.compile(r'\s+')
 
 def extract_command(line):
+    """Get the name of the command executed from a line"""
     pieces = RE_WHITESPACE.split(line)
     if len(pieces) >= 3:
         return pieces[2]
     return None
 
-if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-        lines = f.read().split("\n")
+def analyze_file(fn):
+    """Print the most commonly executed commands of a file"""
+    with open(fn, 'r') as _:
+        lines = _.read().split("\n")
 
-    all_commands = [extract_command(line) for line in lines]
-    clean_commands = [c for c in all_commands if c is not None]
+    all_commands = [extract_command(_) for _ in lines]
+    clean_commands = [_ for _ in all_commands if _ is not None]
 
     counter = Counter(clean_commands)
-    for command, cnt in counter.most_common(100):
+    for command, cnt in counter.most_common():
         print command, cnt
 
+if __name__ == '__main__':
+    analyze_file(sys.argv[1])
